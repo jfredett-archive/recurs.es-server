b006cf554572649865db29c43ea6ad4ded284fee
Joe Fredette <jfredett@gmail.com>
2012-06-20 18:33:39 -0400

    repo contains no more non-server related stuff

---
+++ cookbooks/git/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: git
+# Recipe:: default
+#
+# Copyright 2012, Joe Fredette
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+package 'git' do
+  action[:install]
+end
---
+++ roles/recurses-server.rb
@@ -0,0 +1,4 @@
+name        "recurses-server"
+description "The recurs.es web server"
+run_list    "recipe[git]"
+
