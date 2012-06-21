cf5a27d4ccc0765ff320f129a5c6c6b071012f0a
Joe Fredette <jfredett@gmail.com>
2012-06-21 07:39:33 -0400

    configure rubinius recipe metadata

--- cookbooks/rubinius/metadata.rb
+++ cookbooks/rubinius/metadata.rb
@@ -4,3 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures rubinius"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+depends 'git'
+depends 'rvm'
+
