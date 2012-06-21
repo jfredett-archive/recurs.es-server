b23047d1f47293c0c00e02f8fecbc6a358324aea
Joe Fredette <jfredett@gmail.com>
2012-06-21 07:37:01 -0400

    configure metadata for recurses-site

--- cookbooks/recurses-site/metadata.rb
+++ cookbooks/recurses-site/metadata.rb
@@ -4,3 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures recurses-site"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+depends 'git'
+depends 'rubinius'
+depends 'neo4j'
