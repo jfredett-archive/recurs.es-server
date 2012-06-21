5e33d6c914939540a12bd34f8e5b8a9618483cb2
Joe Fredette <jfredett@gmail.com>
2012-06-21 07:36:48 -0400

    add rubinius recipe

---
+++ cookbooks/rubinius/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Joseph Fredette"
+maintainer_email "jfredett@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures rubinius"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
