278da32a26a631b42d64d7d8afb40bfe4d5aaf7c
Joe Fredette <jfredett@gmail.com>
2012-06-21 16:13:14 -0400

    add Non-linux platforms manifest

--- cookbooks/nginx/metadata.rb
+++ cookbooks/nginx/metadata.rb
@@ -1,6 +1,9 @@
-maintainer       "Joseph Fredette"
-maintainer_email "jfredett@gmail.com"
-license          "All rights reserved"
-description      "Installs/Configures nginx"
+maintainer       'Joseph Fredette'
+maintainer_email 'jfredett@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+depends 'recurses-site', '~> 0.0.1'
+depends 'recurses-users', '~> 0.0.1'
