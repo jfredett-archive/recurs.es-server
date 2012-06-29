c1eeed94a43e03173644ccd0a27b3f854e419a9a
Joe Fredette <jfredett@gmail.com>
2012-06-28 22:43:35 -0400

    add default installed rubies for rvm

---
+++ cookbooks/rvm/attributes/default.rb
@@ -0,0 +1,8 @@
+default['rvm'] = {
+  'rubies' => [
+    'rbx --1.9',
+    #'jruby',
+    #'1.9.3-perf --patch falcon',
+    '1.9.3',
+  ]
+}
--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -7,8 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 
+RVM_PATH='/usr/local/rvm/bin/rvm'
+
 bash 'install RVM as root' do
   user 'root'
   code 'curl -L https://get.rvm.io | sudo bash ; echo'
-  #not_if 'test -e /usr/local/rvm/bin/rvm'
+  not_if "test -e #{RVM_PATH}"
+end
+
+node['rvm']['rubies'].each do |ruby|
+  bash "installing #{ruby}" do
+    user 'root'
+    code "#{RVM_PATH} install #{ruby}"
+  end
 end
