7f71329fab8535ae2e261fb849422033de6a0f7d
Joe Fredette <jfredett@gmail.com>
2012-10-18 23:00:54 -0400

    initial pass on the monit recipe
    still need to factor out some stuff into attributes, 
    and create a provider for monitor-this scripts.

---
+++ cookbooks/monit/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: monit
+# Recipe:: default
+#
+# Copyright 2012, Joseph Fredette
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'monit' do
+  action [:install, :upgrade]
+end
+
+template '/etc/monitrc' do
+  source 'monitrc.erb'
+end
+
+service 'monit' do
+  service_name 'monitd'
+  enabled true
+  action [:enable, :restart]
+  supports [:start, :restart, :stop]
+end
