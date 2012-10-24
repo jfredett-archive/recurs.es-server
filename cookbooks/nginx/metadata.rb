7e23f845c6e6ed5cb9981fcd9535aa6b35e4f762
Joe Fredette <jfredett@gmail.com>
2012-10-24 07:23:09 -0400

    add monit dependency in nginx metadata

--- cookbooks/nginx/metadata.rb
+++ cookbooks/nginx/metadata.rb
@@ -6,5 +6,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 #depends 'recurses-site', '~> 0.0.1'
+depends 'monit', '~> 0.0.1'
 
 supports 'arch'
+
+
