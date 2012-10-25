4b01cf75d1c8ebeed59317a8baef4fbdb3c8279e
Joe Fredette <jfredett@gmail.com>
2012-10-25 08:16:39 -0400

    add base_directories cookbook
    This will create stuff like /var/logs, /var/pids, etc. And
    provide some resources for creating a sudoer, or a logdir, or w/e

---
+++ cookbooks/base_directories/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Joe Fredette"
+maintainer_email "jfredett@gmail.com"
+license          "All rights reserved"
+description      "Sets up base directories for common installs, provides resources for building specific subdirectories"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ cookbooks/base_directories/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: base_directories
+# Recipe:: default
+#
+# Copyright 2012, Joe Fredette
+#
+# All rights reserved - Do Not Redistribute
+#
