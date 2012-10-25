37c1a067673b824db91585ad273be6f8b82f39a8
Joe Fredette <jfredett@gmail.com>
2012-10-25 18:31:11 -0400

    set up installation resources in rvm::default

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -6,3 +6,24 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+
+# THIS MUST GO FIRST
+# -- if it doesn't, then we'll install and then immediately upgrade
+#
+#only when rvm is installed already
+bash 'upgrade rvm to most recent stable version' do
+  user 'root'
+  code 'rvm get stable'
+  only_if 'test -e /user/local/rvm/bin/rvm'
+end
+
+# THIS MUST GO SECOND
+# -- see above
+#
+#only if rvm isn't installed
+bash 'install rvm as root' do
+  user 'root'
+  code 'curl -L https://get.rvm.io | sudo bash ; echo'
+  not_if 'test -e /usr/local/rvm/bin/rvm'
+end
