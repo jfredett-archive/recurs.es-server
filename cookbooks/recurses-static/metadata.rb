741f872236eba008ab66a95de8a6086a1a629eae
Joe Fredette <jfredett@gmail.com>
2012-10-26 17:56:39 -0400

    stub in basics for recurses-static recipe

---
+++ cookbooks/recurses-static/metadata.rb
@@ -0,0 +1,11 @@
+maintainer       "Joe Fredette"
+maintainer_email "jfredett@gmail.com"
+license          "All rights reserved"
+description      "A deploy resource for the recurs.es static site"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+
+depends 'base_directories'
+depends 'git'
+depends 'rvm'
---
+++ cookbooks/recurses-static/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: recurses-static
+# Recipe:: default
+#
+# Copyright 2012, Joe Fredette
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe :base_directories
+include_recipe :rvm
