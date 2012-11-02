0409ffd8b77d0453513f81c87dd79ccf56786c53
Joe Fredette <jfredett@gmail.com>
2012-11-02 16:01:56 -0400

    make the base_dir helpers useful as names in recipes

--- cookbooks/base_directories/libraries/base_directory_helpers.rb
+++ cookbooks/base_directories/libraries/base_directory_helpers.rb
@@ -1,10 +1,11 @@
 class Chef::Recipe
   def log_dir(name, &block)
     base_directories_log name, &block
+    "/var/logs/#{name}"
   end
 
   def site_dir(name, &block)
     base_directories_site name, &block
+    "/srv/www/#{name}"
   end
-
 end
