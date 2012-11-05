10011c2e9c96cbf02d0c070d4960d467f59b115e
Joe Fredette <jfredett@gmail.com>
2012-11-04 21:23:35 -0500

    implement rvm_ruby lwrp

--- cookbooks/rvm/providers/ruby.rb
+++ cookbooks/rvm/providers/ruby.rb
@@ -1,11 +1,26 @@
 action :install do
+  bash "installing #{new_resource.name}" do
+    user 'rvm'
+    code "sudo /usr/local/rvm/bin/rvm install #{new_resource.name} #{new_resource.options.join(' ')}"
+  end
 
+  new_resource.updated_by_last_action(true)
 end
 
 action :remove do
+  bash "uninstalling #{new_resource.name}" do
+    user 'rvm'
+    code "sudo /usr/local/rvm/bin/rvm uninstall #{new_resource.name} #{new_resource.options.join(' ')}"
+  end
 
+  new_resource.updated_by_last_action(true)
 end
 
 action :reinstall do
+  bash "reinstalling #{new_resource.name}" do
+    user 'rvm'
+    code "sudo /usr/local/rvm/bin/rvm reinstall #{new_resource.name} #{new_resource.options.join(' ')}"
+  end
 
+  new_resource.updated_by_last_action(true)
 end
