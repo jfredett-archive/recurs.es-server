9c940503c1ba858199cb42e109b23a2e33cd2399
Joe Fredette <jfredett@gmail.com>
2012-11-10 17:53:15 -0500

    implement rvm_ruby

--- cookbooks/rvm/providers/ruby.rb
+++ cookbooks/rvm/providers/ruby.rb
@@ -1,7 +1,16 @@
 action :install do
   bash "installing #{new_resource.name}" do
     user 'rvm'
-    code "sudo /usr/local/rvm/bin/rvm install #{new_resource.name} #{new_resource.options.join(' ')}"
+    code "/usr/local/rvm/bin/rvmsudo /usr/local/rvm/bin/rvm install #{new_resource.name} #{new_resource.options.join(' ')}"
+  end
+
+  new_resource.global_gems.each do |gem|
+    rvm_execute "installing global gem: #{gem}" do
+      ruby new_resource.name
+      gemset 'global'
+
+      code "gem install #{gem}"
+    end
   end
 
   new_resource.updated_by_last_action(true)
@@ -10,7 +19,7 @@ end
 action :remove do
   bash "uninstalling #{new_resource.name}" do
     user 'rvm'
-    code "sudo /usr/local/rvm/bin/rvm uninstall #{new_resource.name} #{new_resource.options.join(' ')}"
+    code "/usr/local/rvm/bin/rvmsudo /usr/local/rvm/bin/rvm uninstall #{new_resource.name} #{new_resource.options.join(' ')}"
   end
 
   new_resource.updated_by_last_action(true)
@@ -19,7 +28,7 @@ end
 action :reinstall do
   bash "reinstalling #{new_resource.name}" do
     user 'rvm'
-    code "sudo /usr/local/rvm/bin/rvm reinstall #{new_resource.name} #{new_resource.options.join(' ')}"
+    code "/usr/local/rvm/bin/rvmsudo /usr/local/rvm/bin/rvm reinstall #{new_resource.name} #{new_resource.options.join(' ')}"
   end
 
   new_resource.updated_by_last_action(true)
