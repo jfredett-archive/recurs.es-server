ef25152335cb8b2fbc215c8aae2ab6c538d882ad
Joe Fredette <jfredett@gmail.com>
2012-11-16 18:19:05 -0500

    allow specification of CWD, user, environment for rvm_execute

--- cookbooks/rvm/resources/execute.rb
+++ cookbooks/rvm/resources/execute.rb
@@ -3,6 +3,11 @@ default_action :do
 
 attribute :name, default: '', required:false
 
+attribute :cwd
+
+attribute :user, default: 'rvm'
+attribute :environment, default: {}
+
 attribute :code, required: true
 attribute :ruby, required: true
 attribute :gemset, required: true
