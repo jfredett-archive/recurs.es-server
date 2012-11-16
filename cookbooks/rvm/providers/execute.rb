d34d64adc249a11db2b43336eb9149a6f261521f
Joe Fredette <jfredett@gmail.com>
2012-11-15 23:43:07 -0500

    tweak rvm_execute to have a better name for it's bash resource

--- cookbooks/rvm/providers/execute.rb
+++ cookbooks/rvm/providers/execute.rb
@@ -1,7 +1,7 @@
 action :do do
   rvm_string = "#{new_resource.ruby}@#{new_resource.gemset}"
 
-  bash 'executing command' do
+  bash new_resource.name do
     user 'rvm'
     code "/usr/local/rvm/bin/rvmsudo /usr/local/rvm/bin/rvm #{rvm_string} do #{new_resource.code}"
   end
