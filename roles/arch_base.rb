3d7436e808702c04d358628c6a7aa0497cc24230
Joe Fredette <jfredett@gmail.com>
2012-11-28 16:16:18 -0500

    include utility in base setup

--- roles/arch_base.rb
+++ roles/arch_base.rb
@@ -1,7 +1,8 @@
 name        'arch_base'
-description 'Initializes a minimal Arch Linux system (package db update, basic directory layout)'
+description 'Initializes a minimal Arch Linux system (package db update, basic directory layout), also includes utility recipes'
 
 run_list(
   'recipe[arch_init]',
+  'recipe[utility]',
   'recipe[base_directories]'
 )
